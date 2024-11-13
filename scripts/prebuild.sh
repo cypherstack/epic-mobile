902a5acfd5582418112609748163d06a431c920e
sneurlax <sneurlax@gmail.com>
2024-11-13 13:07:36 -0600

    add kGrdrnKey API key to prebuild.sh

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -4,5 +4,9 @@
 KEYS=../lib/external_api_keys.dart
 if ! test -f "$KEYS"; then
     echo 'prebuild.sh: creating template lib/external_api_keys.dart file'
-    printf 'const kChangeNowApiKey = "";\nconst kSimpleSwapApiKey = "";\n' > $KEYS
+else
+    echo 'prebuild.sh: recreating template lib/external_api_keys.dart file'
+    rm $KEYS
 fi
+
+printf 'const kChangeNowApiKey = "";\nconst kSimpleSwapApiKey = "";\nconst kGrdrnKey = "";\n' > $KEYS
