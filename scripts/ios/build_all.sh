aa7cd2df7ef4ca7da809ddd407a50da758d10a61
julian <julian@cypherstack.com>
2024-11-14 16:43:44 -0600

    consistent rust versions

--- scripts/ios/build_all.sh
+++ scripts/ios/build_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if rustup toolchain list | grep -q "1.67.1"; then
+  rustup default 1.67.1
+else
+  echo "Rust version 1.67.1 is not installed. Please install it using 'rustup install 1.67.1'." >&2
+  exit 1
+fi
+
 (cd ../../crypto_plugins/flutter_libepiccash/scripts/ios && ./build_all.sh )  &
 
 wait
